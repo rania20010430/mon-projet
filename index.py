e0d800430aed8081d921e17dfbca119a1376be68
rania20010430 <raniamaamer2001@gmail.com>
2024-12-18 21:36:24 +0100

    Ajout de la fonction produit dans index.py

--- index.py
+++ index.py
@@ -3,6 +3,8 @@ def main():
     a = int(input("Entrez le premier nombre : "))
     b = int(input("Entrez le deuxième nombre : "))
     print(f"La somme de {a} et {b} est {a + b}.")
+def produit(a, b):
+    print(f"Le produit de {a} et {b} est {a * b}.")
 
 if __name__ == "__main__":
     main()
